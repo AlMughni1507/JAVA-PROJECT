e7b167f98c4ab6b650dc40ba30e921ad7ecb119d
Al Mughni <146813597+AlMughni1507@users.noreply.github.com>
2024-08-13 10:24:16 +0700

    Add files via upload

---
+++ pertemuan3no1/src/main/java/com/mycompany/pertemuan3no1/Pertemuan3no1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan3no1;
+
+/**
+ *
+ * @author Mughni
+ */
+public class Pertemuan3no1 {
+    
+        public static void main(String[] args) {
+             // TODO code application logic here
+             int Var1 = 9;
+             float Var2 = 8.99f;
+             char Var3 = 'A';
+             boolean Var4 = false;
+             String Var5 = "Hello World";
+             
+            System.out.println("nilai Var1: " + Var1);
+            System.out.println("nilai Var2: " + Var2);
+            System.out.println("nilai Var3: " + Var3);
+            System.out.println("nilai Var4: " + Var4);
+            System.out.println("nilai Var5: " + Var5);
+        }
+    }
+
